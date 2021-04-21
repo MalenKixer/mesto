7c4c2bd8c90f44bd4172637c3bff58bec83bdf6a
Alexander Orlov <orlov.03.05.2005@yandex.by>
2021-04-21 15:57:56 +0300

    fix: исправить стили и индекс от ревьюра

--- scripts/scritpt.js
+++ scripts/scritpt.js
@@ -1,14 +1,12 @@
-let content = document.querySelector('.content');
-let popup = content.querySelector('.popup');
-let profile = content.querySelector('.profile');
+let popup = document.querySelector('.popup');
+let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit-button');
 let profileTitle = profile.querySelector('.profile__title');
 let profileSubtitle = profile.querySelector('.profile__subtitle');
-let closeButton = popup.querySelector('.close-icon');
+let closeButton = popup.querySelector('.popup__close-icon');
 let formElement = popup.querySelector('.popup__container'); 
 let inputName = formElement.querySelector('.popup__item_text_name');
 let inputActivity = formElement.querySelector('.popup__item_text_activity');
-let submitButton = popup.querySelector('.popup__button');
 function formSubmitHandler(evt){
    evt.preventDefault();
    profileTitle.textContent = inputName.value;
@@ -25,4 +23,4 @@ function openRedactor(){
 }
    editButton.addEventListener('click', openRedactor);
    closeButton.addEventListener('click', closeRedactor);
-   submitButton.addEventListener('click', formSubmitHandler);
+   popup.addEventListener('submit', formSubmitHandler);
